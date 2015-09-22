aa26563f891851e98722b6f637b1de2e208b97e1
Karl-E. Kiel <kekiel@kekiel.de>
2015-09-22 21:48:26 +0200

    Finished first task. Version 1.0 of the app.

--- app/src/main/java/com/kekiel/myappportfolio/MainActivity.java
+++ app/src/main/java/com/kekiel/myappportfolio/MainActivity.java
@@ -2,36 +2,43 @@ package com.kekiel.myappportfolio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * Triggers a toast message onClick.
+     * Will be applied to any button in this view.
+     */
+    private View.OnClickListener anyClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String toastText = getString(R.string.message_this_will_start_app) + ((Button) v).getText().toString();
+            Toast.makeText(getApplicationContext(),toastText, Toast.LENGTH_SHORT).show();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Let the buttons do some action.
+        Button popMoviesButton = (Button) findViewById(R.id.button_popular_movies);
+        popMoviesButton.setOnClickListener(anyClickListener);
+        Button footballAppButton = (Button) findViewById(R.id.button_football_scores);
+        footballAppButton.setOnClickListener(anyClickListener);
+        Button libraryAppButton = (Button) findViewById(R.id.button_library_app);
+        libraryAppButton.setOnClickListener(anyClickListener);
+        Button buildBiggerAppButton = (Button) findViewById(R.id.button_build_it_bigger);
+        buildBiggerAppButton.setOnClickListener(anyClickListener);
+        Button xyzAppButton = (Button) findViewById(R.id.button_xyz_reader);
+        xyzAppButton.setOnClickListener(anyClickListener);
+        Button capstoneAppButton = (Button) findViewById(R.id.button_capstone);
+        capstoneAppButton.setOnClickListener(anyClickListener);
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
